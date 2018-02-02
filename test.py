4d56789efad0cb5cf427f149311a91e6ce66ec21
HugoDelval <hugodelval@gmail.com>
2018-02-02 11:45:06 +0100

    test

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ print("helooo")
 ovh.Client(...)
 # testgf
 # X-Ovh-Signature
-# fake_token = "0123456789azertyuiopqsdfghjklmwx", eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+# fake_token = "0123456789azertyuiopqsdfghjklmwx", eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
